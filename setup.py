0527256642b3e990d7d81ddecb2540e782ff6fee
Vozdyx <51175216+Vozdyx74@users.noreply.github.com>
2019-10-27 16:05:30 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ setup(
     version='0.0.1',
     description='A wrapper for MySQL to keep your code free from SQL.',
     long_description='content will follow',
-    url='https://github.com/Vozdyx74/MySQLib',
-    author='Vozdyx74 ,HaCsO',
+    url='https://github.com/Vozdyx74/SimpleMySQL',
+    author='Vozdyx74, HaCsO',
     license='MIT',
     classifiers=[
         'Programming Language :: Python :: 3.7'
